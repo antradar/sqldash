be04548c2487aa4482e8c548f19495cf3a108253
unknown <schien@antradar.com>
2023-04-30 22:19:24 -0400

    insertion access control

--- icl/addblankrow.inc.php
+++ icl/addblankrow.inc.php
@@ -1,15 +1,23 @@
 <?php
 
+if (!isset($_GET['sqlmode'])||$_GET['sqlmode']!='sqlite') include 'subconnect.php';
+
 function addblankrow(){
 	global $db;
 	
 	global $SQL_ENGINE;
-	
+
+	if (!isset($_GET['sqlmode'])||$_GET['sqlmode']!='sqlite') $dbname=checkdbname();
+		
 	global $sqlite_root;
 
 	$table=GETSTR('table');
 
 	$pkey=GETSTR('pkey');
+	
+	$user=userinfo();
+	
+	if (!isset($user['groups']['insert'])) apperror('Access denied');
 	
 	$query="insert into $table () values ()";
 	$rs=sql_prep($query,$db);
