c81fa9a2d3c01455b8578a2d22457eb158eeccd0
unknown <schien@antradar.com>
2024-11-28 07:51:24 -0500

    better tab drag

--- tabs.js
+++ tabs.js
@@ -244,22 +244,23 @@ function addtab(key,title,params,loadfunc,data,opts){
 	var metakey=0;
 	if (document.keyboard['key_17']||document.keyboard['key_91']||document.keyboard['key_224']) metakey=1;
 	if (!metakey) return;
-	  
-	if (t.reloadinfo&&t.reloadinfo.opts&&t.reloadinfo.opts.noclose) return;
 	
+	if (t.reloadinfo&&t.reloadinfo.opts&&t.reloadinfo.opts.noclose) return;
+	  
 	var shadow=gid('tabshadow');
 	shadow.style.left=t.offsetLeft+'px';
-	shadow.style.top=(t.offsetTop-t.offsetHeight)+'px';
+	shadow.style.top=(t.offsetTop)+'px'; //-t.offsetHeight
 	shadow.innerHTML=t.innerHTML;
 	shadow.style.display='block';
-	shadow.style.opacity=0.5;
+	shadow.style.opacity=0.6;
+	shadow.style.filter='sepia(1)';
 	shadow.style.overflow='hidden';
-	shadow.style.width='80px';
+	//shadow.style.width='80px';
 	var ox=e?e.clientX:event.clientX;
 	var oy=e?e.clientY:event.clientY;
 	
 	var posx=t.offsetLeft;
-	var posy=t.offsetTop-t.offsetHeight;
+	var posy=t.offsetTop;//-t.offsetHeight;
 		
 	document.tabmovesrc=t;
 	t.onmousemove=function(e){
@@ -269,6 +270,27 @@ function addtab(key,title,params,loadfunc,data,opts){
 		var ny=posy+y-oy;
 		shadow.style.left=nx+'px';
 		shadow.style.top=ny+'px';
+		
+		if (document.tabmovedst){
+			document.tabmovedst.style.border='none';
+			document.tabmovedst=null;	
+		}
+
+		var mybox=shadow.getBoundingClientRect();
+				
+		for (var i=0;i<document.tabkeys.length;i++){
+			var dst=document.tabtitles[i];
+			if (t==dst) continue; //skip self
+			if (dst.reloadinfo&&dst.reloadinfo.opts&&dst.reloadinfo.opts.noclose) continue;
+			
+			var box=document.tabtitles[i].getBoundingClientRect();
+
+			if (mybox.x>box.x&&mybox.x<box.x+box.width && mybox.y>box.y&&mybox.y<box.y+box.height){
+	  			document.tabmovedst=dst;
+	  			dst.style.borderLeft='solid 3px #ffab00';
+				break;
+			}
+		}
 		
 	}
 		
@@ -290,18 +312,6 @@ function addtab(key,title,params,loadfunc,data,opts){
 	document.onmouseup=t.onmouseup;
 	
   }
-  
-  t.onmouseover=function(e){
-	  if (document.tabmovedst) document.tabmovedst.style.border='none';
-	  document.tabmovedst=null;
-	  
-	  if (!document.tabmovesrc) return;
-	  if (t==document.tabmovesrc) return;
-	  if (t.reloadinfo&&t.reloadinfo.opts&&t.reloadinfo.opts.noclose) return;
-	  document.tabmovedst=t;
-	  t.style.borderLeft='solid 2px #ffab00';
-	  
-  }  
 
   t.reloadinfo={params:params,loadfunc:loadfunc,data:data,opts:opts};
 
